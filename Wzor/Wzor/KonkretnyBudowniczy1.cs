6b3527db1994f348084facfc756f232cbe7cf4db
ChrisCoderPoland <40177095+ChrisCoderPoland@users.noreply.github.com>
2018-11-21 18:34:08 +0100

    0.0.1

--- Wzor/Wzor/KonkretnyBudowniczy1.cs
+++ Wzor/Wzor/KonkretnyBudowniczy1.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Wzor
 {
-    class KonkretnyBudowniczy1
+    class KonkretnyBudowniczy1 : Budowniczy
     {
+
+        public Order OrderInstance { get; private set; }
+
+        public KonkretnyBudowniczy1()
+        {
+            OrderInstance = new Order();
+            OrderInstance.Punkty = false;
+        }
+
+
+
+
+        public override void CreatAddress(string address)
+        {
+
+            OrderInstance.Address = address;
+        }
+
+    public override void CreatName(string Name)
+        {
+            OrderInstance.RecipientName = Name;
+        }
+
+        public override void CreatOrder(List<Book> booksOrder)
+        {
+            OrderInstance.BooksOrder = booksOrder;
+
+        }
     }
 }
